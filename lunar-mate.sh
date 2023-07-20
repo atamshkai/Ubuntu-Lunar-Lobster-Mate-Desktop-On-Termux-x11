8e607d58755075a0d2c184f13bf1f39cf9d6d8d2
atamshkai <66311425+atamshkai@users.noreply.github.com>
2023-07-20 17:34:26 +0630

    Add files via upload

---
+++ lunar-mate.sh
@@ -0,0 +1,12 @@
+echo "Install needed packages"
+sleep 2
+pkg up -y && pkg i -y x11-repo && pkg i -y proot-distro pulseaudio termux-x11-nightly
+cat <<EOF > ~/../usr/etc/proot-distro/lunar-mate.sh
+DISTRO_NAME='Lunar Lobster (lunar mate)'
+TARBALL_URL['aarch64']="https://archive.org/download/lunar-mate.tar/lunar-mate.tar.xz"
+TARBALL_SHA256['aarch64']="a220121f0848707536fbdfa252157d31913584f9b64ac149fbd57e3d65eab7be"
+EOF
+echo "proot-distro login user mate lunar-mate --shared-tmp --bind /dev/null:/proc/sys/kernal/cap_last_cap" >>~/../usr/bin/lunar-mate
+chmod +x ~/../usr/bin/lunar-mate
+proot-distro install lunar-mate
+
